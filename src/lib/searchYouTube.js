16d98268ac9017ccf5211aa7667cd2e5da3123f1
cwillens <charwillens@gmail.com>
2016-09-30 19:44:53 -0700

    all youtube tests passing

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,22 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet',
+      videoEmbeddable: 'true',
+      type: 'video'
+    },
+    success: function (data) {
+      callback(data.items);
+    },
+    error: function (data) {
+      console.error('Failed to get data', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
