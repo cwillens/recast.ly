5257e3ed4ffff1dc6001b6a86557679a1d599b18
cwillens <charwillens@gmail.com>
2016-09-30 17:38:23 -0700

    changing video on click is working, previous tests are destroyed

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,13 +1,10 @@
 var VideoList = (props) => (
   <div className="video-list media">
     {props.videos.map(video =>
-      <VideoListEntry video={video} />
+      <VideoListEntry video={video} handleClick={props.handleClick} />
     )}
   </div>
 );
-
-
-
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
